2117d6cdf157797191e68c4a5c820bf940f636cf
Daniel Pelánek <dan@Daniels-MacBook-Air.local>
2023-01-11 22:09:24 +0100

    remove files

--- tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-    for(int i=0; i<y/2; i++){
-        for(int j=0; j<x; j++){
-            int temp = pole[j][i];
-            pole[j][i] = pole[j][y-i-1];
-            pole[j][y-i-1] = temp;
-        }
-    }
