16f0d0aa9933dc02ddc7180efd90344c4c3aabf4
Daniel Pelánek <dan@Daniels-MacBook-Air.local>
2023-01-14 12:49:41 +0100

    oprava nacitani

--- 04strukturovanySoubor.c
+++ 04strukturovanySoubor.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct sopka{
     char nazev[100];
@@ -36,27 +38,29 @@ void pridejSopku(Tseznam *s, Tsopka *a){
 }
 
 void nactiSoubor(Tseznam* sopky, FILE *f){
-    char aktivni[3], jmeno[100];
+    char aktivni[4], jmeno[100];
     int vyska;
-    while(fscanf(f, "%99s;%d;%s", jmeno, &vyska, aktivni) == 3){
+    while(fscanf(f,"%99s %d %3s", jmeno, &vyska, aktivni) == 3){
+        fflush(stdout);
         Tsopka* temp = malloc(sizeof(Tsopka));
         strcpy(temp->nazev, jmeno);
         temp->vyska = vyska;
         if(aktivni[0] == 'A') temp->aktivni = 1;
         else temp->aktivni = 0; 
-
         pridejSopku(sopky, temp);
     }
 }
 
 //Vytvoř funkci vracející průměrnou výšku všech ne/aktivních sopek (vybere se pa-rametrem).
 float prumerVysek(Tseznam *s, bool aktivni){
-    int soucet;
+    int soucet=0, pocet=0;
     for(int i=0; i<s->delka; i++){
-        if(s->sopky[i]->aktivni)
+        if(s->sopky[i]->aktivni == aktivni){
+            pocet++;
             soucet += s->sopky[i]->vyska;
+        }
     }
-    return soucet / (s->delka * 1.0);
+    return soucet / (pocet * 1.0);
 }
 
 //Vypiš názvy všech sopek vyšších než hodnota zadaná parametrem. U každého
@@ -75,8 +79,8 @@ void jeVyssi(Tseznam *s, int vyska){
 //Vytvoř jeden soubor se záznamy o aktivních a druhý se záznamy o neaktivních
 //sopkách. Výstupní soubory musí zachovávat formát vstupního souboru.
 void vypisSopkuSoubor(Tsopka *s, FILE *f){
-    if(s->aktivni) fprintf(f ,"%s;%d;ANO", s->nazev, s->vyska);
-    else fprintf(f, "%s;%d;NE", s->nazev, s->vyska);
+    if(s->aktivni) fprintf(f ,"%s %d ANO\n", s->nazev, s->vyska);
+    else fprintf(f, "%s %d NE\n", s->nazev, s->vyska);
 }
 
 void dvaSoubory(Tseznam *s){
@@ -92,8 +96,8 @@ void dvaSoubory(Tseznam *s){
 int main(){
     Tseznam* sopky = vytvorSeznam();
     char cesta[100];
-    scanf("%99s", cesta);
-    FILE *f = fopen(cesta, "r");
+    // scanf("%99s", cesta);
+    FILE *f = fopen("sopky.txt", "r");
     if(f == NULL) return 1;
     nactiSoubor(sopky, f);
     return 0;
