c6990a450595d215972776a2bbb775eb357680d1
Daniel Pelánek <dan@Daniels-MacBook-Air.local>
2023-04-04 18:58:45 +0200

    pridani Jacobiho metody

--- 15maticeStruktury.c
+++ 15maticeStruktury.c
@@ -3,25 +3,27 @@
 #include <stdlib.h>
 #include <math.h>
 
+#define ODCHYLKA 0.001
+
 typedef struct pole
 {
   float **prvek;
   int vyska;
-  int delka;
+  int sirka;
 }Tmatice;
 
-Tmatice * vytvorM(int vyska, int delka){
+Tmatice * vytvorM(int vyska, int sirka){
     Tmatice *m = malloc(sizeof(Tmatice));
     if(m == NULL) return NULL;
     m->vyska = vyska;
-    m->delka = delka;
+    m->sirka = sirka;
 
     //alokace samotné matice
     m->prvek = malloc(m->vyska * sizeof(float*));
     if(m->prvek == NULL) return NULL;
     for(int i=0; i<m->vyska; i++){
         //alokace řádku
-        m->prvek[i] = malloc(m->delka * sizeof(float));
+        m->prvek[i] = malloc(m->sirka * sizeof(float));
         if(m->prvek[i] == NULL) return NULL;
     }
     return m;
@@ -35,15 +37,15 @@ void smazM(Tmatice* m){
 
 //Načti ze vstupu matici koeficientů o rozměrech m×n.
 Tmatice* nactiM(){
-    int vyska, delka;
-    scanf("%d %d", &vyska, &delka);
-    if(vyska+1 != delka) return NULL;
+    int vyska, sirka;
+    scanf("%d %d", &vyska, &sirka);
+    if(vyska+1 != sirka) return NULL;
 
-    Tmatice *m = vytvorM(vyska,delka);
+    Tmatice *m = vytvorM(vyska,sirka);
     if(m == NULL) return NULL;
 
     for (int i=0; i<vyska; i++){
-        for(int j=0; j<delka; j++){
+        for(int j=0; j<sirka; j++){
             float prvek;
             scanf("%f", &prvek);
             m->prvek[i][j] = prvek;
@@ -55,7 +57,7 @@ Tmatice* nactiM(){
 // Ověř, zda načtená matice může být maticí koeficientů soustavy lineárních rovnic
 // (stačí rozměry).
 bool jeSoustava(Tmatice* m){
-    if(m->vyska+1 != m->delka) return false;
+    if(m->vyska+1 != m->sirka) return false;
     return true;
 }
 
@@ -73,6 +75,54 @@ bool jeDominantni(Tmatice* m){
         if(fabs(m->prvek[i][i]) <= soucet) return false;
     }
     return true;
+}
+
+bool vynuluj(Tmatice *m){
+    for(int i=0; i<m->vyska; i++){
+        for(int j=0; j<m->sirka; j++){
+            if(i == j) continue;
+            m->prvek[i][j] /= m->prvek[i][i];
+        }
+        m->prvek[i][i] = 0;
+    }
+    return 1;
+}
+
+bool odchylka(float res[][2], int n){
+    for(int i=0; i<n; i++)
+        if(fabs(fabs(res[i][0]) - fabs(res[i][1])) > ODCHYLKA) return false;
+
+    return true;
+}
+
+void Jacob(Tmatice *m){
+    float res[m->vyska][2];
+    for(int i=0; i<m->vyska; i++){ res[i][0] = 0; res[i][1] = 0; }
+
+    if(!diagonalneDominantni(m)) return;
+
+    vynuluj(m);
+
+    //prubeh metody
+    do {
+        //prekopirovani vysledku z druheho do prvniho pole
+        for(int i=0; i<m->vyska; i++)
+            res[i][0] = res[i][1];
+
+        for(int i=0; i<m->vyska; i++){
+            float b = m->prvek[i][m->vyska];
+
+            for(int j=0; j<m->vyska; j++)
+                b -= m->prvek[i][j] * res[j][0];
+
+            res[i][1] = b;
+        }
+    } while(!odchylka(res, m->vyska));
+
+    //vysledky
+    printf("\n");
+    for (size_t i=0; i<m->vyska; i++)
+        printf("%d.x: %f\n", i, res[i][1]);
 }
 
 int main(){
