c7054dea83138d7d3ec988a5d195761d3f7b9543
Daniel Pelanek <72805357+Siigull@users.noreply.github.com>
2023-08-26 15:52:41 +0200

    Update 25adresy2.c
    added ipv4 check

--- 25adresy2.c
+++ 25adresy2.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 // Ověř zda je zadaný textový řetězec validní MAC, IPv4, IPv6, e-mailovou adresou.
 // Kontroluj oddělovače, přebytečné nuly, mezery atd.
@@ -16,6 +19,30 @@ bool jeMAC(char* vstup){
     return 1;
 }
 
+bool is_number(char* ip, int len) {
+    for(int i=0; i<len; i++) 
+        if(!isdigit(ip[i])) return false;
+
+    if(atoi(ip) > 255) return false;
+
+    return true;
+}
+
+bool is_ip(char* ip) {
+    for(int i=0; i<4; i++) {
+        int x = strcspn(ip, ".\0");
+
+        if(ip[-1] == '\0') return false;
+
+        if(!is_number(ip, x)) return false; 
+
+        ip += x + 1;
+    }
+
+    if(ip[-1] != '\0' || ip[-2] == '.') return false;
+
+    return true;
+}
 
 int main(){
     return 0;
